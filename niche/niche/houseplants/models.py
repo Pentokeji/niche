96b0b0441a23322a301251f72218394ea06d29ba
Tobias Weisskopf <tobias.weisskopf.ch@gmail.com>
2020-07-25 19:41:15 +0200

    Testing QR

--- niche/niche/houseplants/models.py
+++ niche/niche/houseplants/models.py
@@ -3,19 +3,18 @@ from django.db import models
 
 # Create your models here.
 class Plant(models.Model):
-    # english_name = models.CharField(max_length=200)
-    # chinese_name = models.CharField(max_length=200)
-    # scientific_name = models.CharField(max_length=200)
+    # english_name = models.TextField(default="", max_length=200)
+    # chinese_name = models.TextField(default="", max_length=200)
+    scientific_name = models.TextField(max_length=200)
     # date_purchased = models.DateTimeField('date purchased')
-    # full_sun = models.BinaryField(default=0)
-    # half_sun = models.BinaryField(default=1)
-    # shadow = models.BinaryField(default=0)
+    # full_sun = models.BooleanField(default=False)
+    # half_sun = models.BooleanField(default=False)
+    # shadow = models.BooleanField(default=False)
     # min_temp = models.IntegerField(default=15)
     # max_temp = models.IntegerField(default=42)
     # comfortable_temp = models.IntegerField(default=25)
-    # family = models.CharField(max_length=200)
-    # comments = models.CharField(max_length=1000)
+    # family = models.TextField(default="", max_length=200)
+    # comments = models.TextField(default="", max_length=1000)
 
     def __str__(self):
-        return self.english_name
-
+        return self.scientific_name
--- niche/niche/houseplants/views.py
+++ niche/niche/houseplants/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
+
 
 # Testing View, will dissapear later.
 def index(request):
-    return HttpResponse("We can keep track of all the plants here.")
+    return HttpResponse("Zamioculas Zamifolia")
 
